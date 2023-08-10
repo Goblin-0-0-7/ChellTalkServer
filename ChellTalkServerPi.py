69f9bdaa030585acfeb7e548a997d772a3167f95
Goblin <mc.goblin@t-online.de>
2023-08-10 18:48:54 +0200

    added co2sensor to ChellTalkServerPi

--- ChellTalkServerPi.py
+++ ChellTalkServerPi.py
@@ -23,7 +23,7 @@ motion_sensor_flag = False
 PIN_RED = 27
 PIN_GREEN = 22
 PIN_BLUE = 17
-PIN_MOTIONSENSOR_OUTPUT = 14
+PIN_MOTIONSENSOR_OUTPUT = 23
 
 
 pwm_frequency = 100
@@ -44,7 +44,7 @@ advertise_service( server_sock, "ChellTalkServer",
                    profiles = [ SERIAL_PORT_PROFILE ]
                     )
 
-def open_socket(connection: bool, alarm_clock: AlarmClock, motion_sensor: MotionSensor):
+def open_socket(connection: bool, alarm_clock: AlarmClock, motion_sensor: MotionSensor, co2_sensor: CO2Sensor):
     while True:
         if(connection == False):
             print("Waiting for connection on RFCOMM channel %d" % port)
@@ -60,19 +60,12 @@ def open_socket(connection: bool, alarm_clock: AlarmClock, motion_sensor: Motion
                 client_sock.close()
                 connection = False
                 led.set_color(0,0,0)
+            #fade
             elif ("-fade-" in data):
                 led.set_color(0,0,0, pwms)
                 fading = Fading(pwms)
                 fade_thread = Thread(target = fading.fade_colors, args =( ), daemon = True)
                 fade_thread.start()
-            elif ("-motionSensor-" in data):
-                if (":activate" in data):
-                    motion_sensor.activate()
-                    motion_sensor_flag = True
-                if (":deactivate" in data): #error when having the if statment as (":deactivate" in data and motion_sensor_flag)
-                    if(motion_sensor_flag):
-                        motion_sensor.deactivate()
-                        motion_sensor_flag = False
             elif ("-fadeSpeed-" in data):
                 msg = data.replace("-fadeSpeed-", "")
                 try:
@@ -89,6 +82,7 @@ def open_socket(connection: bool, alarm_clock: AlarmClock, motion_sensor: Motion
                 except:
                     print("Stroboscope could not be terminated.")
                 led.set_color(0,0,0, pwms)
+            #strobe
             elif ("-strobe-" in data):
                 strobe = Stroboscope(pwms)
                 strobe_thread = Thread(target = strobe.run, args =( ), daemon = True)
@@ -99,10 +93,12 @@ def open_socket(connection: bool, alarm_clock: AlarmClock, motion_sensor: Motion
                     strobe.change_frequency(int(msg))
                 except:
                     print("Couldn't change fading speed, fading probably not active.")
+            #rgb
             elif ("-rgbCode-" in data):
                 msg = data.replace("-rgbCode-", "") #removing Tag
                 red, green, blue, __ = msg.split(",",3)
                 led.set_color(int(red), int(green), int(blue), pwms)
+            #alarm
             elif ("-newTestAlarm-" in data):
                 alarm_clock.new_test_alarm()
             elif ("-newAlarm-" in data):
@@ -111,6 +107,18 @@ def open_socket(connection: bool, alarm_clock: AlarmClock, motion_sensor: Motion
                 alarm_clock.new_alarm(title, [int(hour), int(min), int(sec)])
             elif ("-stopAlarm-" in data):
                 alarm_clock.stop_alarms()
+            #sensors
+            elif ("-motionSensor-" in data):
+                if (":activate" in data):
+                    motion_sensor.activate()
+                    motion_sensor_flag = True
+                if (":deactivate" in data): #error when having the if statment as (":deactivate" in data and motion_sensor_flag)
+                    if(motion_sensor_flag):
+                        motion_sensor.deactivate()
+                        motion_sensor_flag = False
+            elif ("-co2Sensor-" in data):
+                if (":calibrate" in data):
+                    co2_sensor.calibrate()
         except IOError:
             print("Connection disconnected!")
             client_sock.close()
@@ -138,7 +146,7 @@ def main():
     
     motion_sensor = MotionSensor(PIN_MOTIONSENSOR_OUTPUT, pwms)
     
-    open_socket(connection, alarm_clock, motion_sensor)
+    open_socket(connection, alarm_clock, motion_sensor, co2_sensor)
     exit()
     
 if __name__ == "__main__":
